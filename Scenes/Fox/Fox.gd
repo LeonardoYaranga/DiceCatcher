5bab74f2b3b9bf9eb094ab06df7d8b635d76bb4b
LeonardoYaranga <leonardo.yaranga@gmail.com>
2026-05-27 17:32:14 -0500

    feat: -Added MCP Server Asset - Added behavior when catch a Dice by the box: score in UI -  Added music and game over sound - Modified the pause beavior, separating the fox and SpawnTimer like pausables in a Node Puasable

--- Scenes/Fox/Fox.gd
+++ Scenes/Fox/Fox.gd
@@ -1,13 +1,26 @@
 extends Area2D
 
 class_name Fox
+signal dice_caught
 
 @export var speed: float = 200.0
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sounds: AudioStreamPlayer2D = $Sounds
 
 func _physics_process(delta: float) -> void:
-	var move: float = 0.0
-	if Input.is_action_pressed("ui_right"):
-		move = speed
-	if Input.is_action_pressed("ui_left"):
-		move = - speed
-	position.x += move * delta
+	#var move: float = 0.0
+	var move: float = Input.get_axis("ui_left","ui_right")
+	#if Input.is_action_pressed("ui_right"):
+		#move = speed
+	#if Input.is_action_pressed("ui_left"):
+		#move = - speed
+	if !is_zero_approx(move):
+		sprite_2d.flip_h = (move > 0.0) 
+	position.x += move * delta * speed
+	
+func _on_area_entered(area: Area2D) -> void:
+	if area is Dice:
+		sounds.play()
+		area.queue_free()
+		dice_caught.emit()
+	
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -2,17 +2,32 @@ extends Node2D
 
 #escena dice
 const DICE = preload("uid://c6wsbyjga68up")
-@onready var spawn_timer: Timer = $SpawnTimer
-const MARGIN: float = 80.0
+const GAME_OVER = preload("uid://c0orcx0ncovyq")
+
+@onready var spawn_timer: Timer = $Pausable/SpawnTimer
+@onready var fox: Fox = $Pausable/Fox
+@onready var score_label: Label = $ScoreLabel
+@onready var music: AudioStreamPlayer = $Music
+@onready var pausable: Node = $Pausable
+
+const MARGIN: float = 80.
 const STOPPABLE_GROUP: String = "stoppable"
+
+var _points: int = 0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_tree().paused = false
 	spawn_timer.timeout.connect(spawn_dice)
 	spawn_timer.start()
+	fox.dice_caught.connect(_on_dice_caught)
+
+func update_score_label() -> void:
+	score_label.text = "%04d" % _points
+	print("_points", _points)
 	
 func spawn_dice() -> void:
 	var new_dice: Dice = DICE.instantiate()
@@ -20,14 +35,22 @@ func spawn_dice() -> void:
 	var screen_vpr: Rect2 = get_viewport_rect()
 	var random_x: float = randf_range(screen_vpr.position.x + MARGIN, screen_vpr.end.x - MARGIN)
 	new_dice.position = Vector2(random_x, -MARGIN)
-	add_child(new_dice)
+	pausable.add_child(new_dice)
 
 func pause_all() -> void:
 	var to_stop: Array[Node] = get_tree().get_nodes_in_group(STOPPABLE_GROUP)
 	for node in to_stop:
 		node.set_physics_process(false)
 	spawn_timer.stop()
+	
+func _on_dice_caught() -> void:
+	_points += 1
+	
 		
 func _on_game_over() -> void:
 	print("Game Over")
-	pause_all()
+	#pause_all()
+	music.stop()
+	music.stream = GAME_OVER
+	music.play()
+	get_tree().paused = true
